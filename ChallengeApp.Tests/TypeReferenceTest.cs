3d1ac7e73a17a51cda29caa7d1973461a81566f2
Wojtek Pugowski <wojtek@pugowski.pl>
2023-05-07 15:24:34 +0200

    Added .gitignore

--- ChallengeApp.Tests/TypeReferenceTest.cs
+++ ChallengeApp.Tests/TypeReferenceTest.cs
@@ -2,5 +2,32 @@ namespace ChallengeApp.Tests;
 
 public class TypeReferenceTest
 {
-    
+    [Test]
+    public void Test1()
+    {
+        //arrange
+        var employee1 = new Employee("Wojtek", "Pugowski");
+        var employee2 = new Employee("Monika", "Pugowska");
+
+        //act
+        //assert
+        Assert.AreNotEqual(employee1, employee2);
+    }
+
+    [Test]
+    public void Test2()
+    {
+        //arrange
+        var employee1 = new Employee("Wojtek", "Pugowski");
+        var employee2 = new Employee("Wojtek", "Pugowski");
+
+        //act
+        //assert
+        Assert.AreEqual(employee1.ToString(), employee2.ToString());
+    }
+
+    private Employee GetEmployee(string name, string surname)
+    {
+        return new Employee(name, surname);
+    }
 }
--- ChallengeApp.Tests/TypeValueTest.cs
+++ ChallengeApp.Tests/TypeValueTest.cs
@@ -2,5 +2,15 @@ namespace ChallengeApp.Tests;
 
 public class TypeValueTest
 {
-    
+    [Test]
+    public void Test1()
+    {
+        //arrange
+        var number1 = 12;
+        var number2 = 12;
+
+        //act
+        //assert
+        Assert.AreEqual(number1, number2);
+    }
 }
